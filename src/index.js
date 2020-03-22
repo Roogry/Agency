d3f73d99bf40dcb1a47338c355a81ebac9a06de3
Roogry <jodiemantra@gmail.com>
2020-03-22 19:57:38 +0800

    responsive progress

--- src/index.js
+++ src/index.js
@@ -1,51 +1,50 @@
 import Swiper from 'swiper';
 import './styles/main.scss';
-import './carausel';
 
 var swiperCLient = new Swiper('.hm-clients__swiper', {
-    slidesPerView: 3,
-    spaceBetween: 50,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-        nextEl: '.hm-clients__next',
-        prevEl: '.hm-clients__prev',
-    },
+  slidesPerView: 3,
+  spaceBetween: 50,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+      nextEl: '.hm-clients__next',
+      prevEl: '.hm-clients__prev',
+  },
 });
 
 var swiperPortfolio = new Swiper('.hm-portfolio__swiper', {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-        nextEl: '.hm-portfolio__next',
-        prevEl: '.hm-portfolio__prev',
-    },
+  slidesPerView: 1,
+  spaceBetween: 50,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+      nextEl: '.hm-portfolio__next',
+      prevEl: '.hm-portfolio__prev',
+  },
 });
 
 var swiperPortfolio = new Swiper('.hm-testimonials__swiper', {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    loop: true,
-    loopFillGroupWithBlank: true,
-    navigation: {
-        nextEl: '.hm-testimonials__next',
-        prevEl: '.hm-testimonials__prev',
-    },
+  slidesPerView: 1,
+  spaceBetween: 50,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+      nextEl: '.hm-testimonials__next',
+      prevEl: '.hm-testimonials__prev',
+  },
 });
